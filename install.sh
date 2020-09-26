819ba3176c12d5d0e986a43fcda509d0d59cfe8d
Mikhail Belov <mpbelov@gmail.com>
2020-09-27 00:52:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,6 +25,8 @@ curl https://raw.githubusercontent.com/mpbelov/Configs/master/.config/.p10k.zsh 
 
 # sudo apt-get install fonts-powerline
 
+sudo apt install tilix
+
 # install stores
 sudo snap install snap-store
 # fix kde problems with snap applications https://www.reddit.com/r/kde/comments/9pjos2/snaps_in_application_launcher/eh0v1um/

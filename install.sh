18a7882c9cd99e7710b100390a56004f2d70d6d6
Mikhail Belov <mpbelov@gmail.com>
2020-09-26 22:25:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
-# install snap store
-sudo snap install snap-store
+# install zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# restart
+
+sudo apt install git
+sudo apt install curl
 
 # install oh-my-zsh https://github.com/ohmyzsh/ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# install zsh plugin zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # install zsh theme powerlevel10k https://github.com/romkatv/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# install zsh plugin zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# install stores
+sudo snap install snap-store
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+flatpak install flathub org.telegram.desktop

46b196a20c58cae5a2a15e162f4026a184397649
Mikhail Belov <mpbelov@gmail.com>
2020-09-27 00:33:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ sudo apt-get install fonts-powerline
 
 # install stores
 sudo snap install snap-store
+# fix kde problems with snap applications https://www.reddit.com/r/kde/comments/9pjos2/snaps_in_application_launcher/eh0v1um/
 sudo sh -c "echo emulate sh -c \'source /etc/profile\' >> /etc/zsh/zprofile"
 
 sudo apt install flatpak

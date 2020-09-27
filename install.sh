7a6775d173e87db742a212eddb89c5d22a79121d
Mikhail Belov <mpbelov@gmail.com>
2020-09-27 22:49:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ chsh -s $(which zsh)
 
 # restart
 
-sudo apt install git
-sudo apt install curl
+sudo apt install -y git
+sudo apt install -y curl
 
 # install oh-my-zsh https://github.com/ohmyzsh/ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -25,7 +25,7 @@ curl https://raw.githubusercontent.com/mpbelov/Configs/master/.config/.p10k.zsh 
 
 # sudo apt-get install fonts-powerline
 
-sudo apt install tilix
+sudo apt install -y tilix
 # fix tilix https://gnunn1.github.io/tilix-web/manual/vteconfig/
 sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 
@@ -34,7 +34,7 @@ sudo snap install snap-store
 # fix kde problems with snap applications https://www.reddit.com/r/kde/comments/9pjos2/snaps_in_application_launcher/eh0v1um/
 sudo sh -c "echo emulate sh -c \'source /etc/profile\' >> /etc/zsh/zprofile"
 
-sudo apt install flatpak
+sudo apt install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists kdeapps --from https://distribute.kde.org/kdeapps.flatpakrepo
 
@@ -44,19 +44,17 @@ flatpak install flathub org.keepassxc.KeePassXC
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt update
-sudo apt install sublime-merge
+sudo apt update && sudo apt install -y sublime-merge
 
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
 echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
-sudo apt update
-sudo apt install albert
+sudo apt update && sudo apt install -y albert
 
 sudo snap install --classic code
 
-sudo apt install tmux
-sudo apt install neovim
+sudo apt install -y tmux
+sudo apt install -y neovim
 
 curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs

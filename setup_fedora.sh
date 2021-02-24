de4fe4856f3396d191082399b281f7d667deb2f2
Mikhail Belov <mpbelov@gmail.com>
2021-02-24 20:09:18 +0000

    Update setup_fedora.sh

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -28,6 +28,36 @@ rm /home/mikhail/temp/NotoSans.zip
 
 sudo fc-cache -f
 
+# Install GUI dconf editor
+sudo dnf install dconf-editor -y
+
+# Add world clocks
+gsettings set org.gnome.shell.world-clocks locations "[<(uint32 2, <('Moscow', 'UUWW', true, [(0.97127572873484425, 0.65042604039431762)], [(0.97305983920281813, 0.65651530216830811)])>)>, <(uint32 2, <('Vancouver', 'CYVR', true, [(0.85841109795478021, -2.1496638678574467)], [(0.85957465660720722, -2.1490820798045869)])>)>]"
+
+
+# Install tilix
+sudo dnf install tilix -y
+sudo dnf remove gnome-terminal -y
+
+# change hotkeys 
+gsettings set com.gexperts.Tilix.Keybindings session-switch-to-next-terminal 'disabled'
+gsettings set com.gexperts.Tilix.Keybindings session-switch-to-previous-terminal 'disabled'
+gsettings set com.gexperts.Tilix.Keybindings win-switch-to-next-session '<Ctrl>Tab'
+gsettings set com.gexperts.Tilix.Keybindings win-switch-to-previous-session '<Ctrl><Shift>Tab'
+gsettings set com.gexperts.Tilix.Keybindings app-new-session '<Ctrl>T'
+gsettings set com.gexperts.Tilix.Keybindings app-new-window '<Ctrl>N'
+gsettings set com.gexperts.Tilix.Keybindings terminal-close '<Ctrl>W'
+gsettings set com.gexperts.Tilix.Keybindings terminal-copy '<Alt>c'
+gsettings set com.gexperts.Tilix.Keybindings terminal-paste '<Alt>v'
+gsettings set com.gexperts.Tilix.Keybindings terminal-find '<Ctrl>f'
+gsettings set com.gexperts.Tilix.Keybindings terminal-find-next '<Ctrl>f'
+gsettings set com.gexperts.Tilix.Keybindings terminal-find-previous '<Ctrl><Shift>f'
+gsettings set com.gexperts.Tilix.Settings prompt-on-close false
+gsettings set com.gexperts.Tilix.Settings terminal-title-style 'none'
+gsettings set com.gexperts.Tilix.Settings theme-variant 'dark'
+gsettings set com.gexperts.Tilix.Settings use-tabs true
+gsettings set com.gexperts.Tilix.Settings window-save-state true
+
 # Install VS Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'

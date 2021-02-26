a2f930a09dc3c9135c0bd3ac2f9b2fd439772453
Mikhail Belov <mpbelov@gmail.com>
2021-02-26 21:10:27 +0000

    Update setup_fedora.sh

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -81,3 +81,5 @@ sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 sudo dnf install sublime-merge -y
 
+# Install KeePassXC
+sudo dnf install keepassxc -y
